21b6808523c26d4c5c3269bbfefda5d7d48e9027
rexa6 <visastoner@gmail.com>
2025-10-31 02:57:56 -1000

    Update CORS

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["https://web-3-informatyk.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
